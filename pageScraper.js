8fecd2593d72882258800c9b91fe4f8219ea6a07
vadimstasiev <vadim444@hotmail.com>
2021-06-22 20:24:57 +0100

    prevent crash

--- pageScraper.js
+++ pageScraper.js
@@ -9,7 +9,7 @@ const scrapingActions = [
             let page = await browser.newPage();
             console.log(`Navigating to ${this.url}...`);
             // Navigate to the selected page
-            await page.goto(this.url, {waitUntil: 'networkidle2'});
+            await page.goto(this.url, { waitUntil: 'networkidle2' });
 
             // Email and password input and login
             const emailInputXPath = '//*[@id="session_key"]';
@@ -21,7 +21,7 @@ const scrapingActions = [
             const [passwordInputHandle] = await page.$x(passwordInputXPath);
             const [signInButtonHandle] = await page.$x(signInXPath);
 
-            
+
             await page.evaluate(async (emailInput, passwordInput, email, password) => {
                 emailInput.value = email;
                 passwordInput.value = password
@@ -40,11 +40,11 @@ const scrapingActions = [
     },
     {
         url: config.target_profile,
-        async scraper(browser){
+        async scraper(browser) {
             let page = await browser.newPage();
             console.log(`Navigating to ${this.url}...`);
             // Navigate to the url
-            await page.goto(this.url, {waitUntil: 'networkidle2'});
+            await page.goto(this.url, { waitUntil: 'networkidle2' });
             // find connections link
             const connectionsLinkXPath = '//span[contains(., "connections")]';
             await page.waitForXPath(connectionsLinkXPath);
@@ -55,28 +55,28 @@ const scrapingActions = [
             sleep(2);
 
             let nextPage = true;
-            while(nextPage){
-                await page.evaluate( () => {
-                    setInterval(function() { window.scrollTo(0, document.body.scrollHeight); }, 100);
+            while (nextPage) {
+                await page.evaluate(() => {
+                    setInterval(function () { window.scrollTo(0, document.body.scrollHeight); }, 100);
                 });
                 nextPage = await page.evaluate(async () => {
                     // if (typeof run !== "undefined") {
                     //     // means the page hasn't refreshed, so the code must have already ran
                     //     return false;
                     // }
-                    
+
                     sleep = (time) => {
                         return new Promise((resolve) => setTimeout(resolve, time));
                     }
                     await sleep(2000)
                     let connectBtns = [...document.querySelectorAll("span")].filter(a => a.textContent.trim() === "Connect".trim())
-                    for (var i=0; i < connectBtns.length; i++) {
+                    for (var i = 0; i < connectBtns.length; i++) {
                         await sleep(1000)
                         connectBtns[i].click()
                         console.log(connectBtns)
                         await sleep(1000)
                         let sendBtns = [...document.querySelectorAll("span")].filter(a => a.textContent.trim() === "Send".trim())
-                        if(sendBtns[0]){
+                        if (sendBtns[0]) {
                             sendBtns[0].click()
                             console.log('did it with', sendBtns[0])
                         }
@@ -89,7 +89,10 @@ const scrapingActions = [
                 const nextPageXPath = '//span[contains(., "Next")]';
                 const [nextPageHandle] = await page.$x(nextPageXPath);
 
-                await nextPageHandle.click();
+                try {
+                    await nextPageHandle.click();
+
+                } catch (error) { }
 
                 console.log('say what, its done')
                 page.waitForNavigation({ waitUntil: 'networkidle2' })
