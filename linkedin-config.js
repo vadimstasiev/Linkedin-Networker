c4a3227f1daedb0866964f3873a55b7e51123d1d
AName* name("Vadim") <vadim444@hotmail.com>
2022-02-19 16:26:46 +0000

    Create linkedin-config.js

---
+++ linkedin-config.js
@@ -0,0 +1,9 @@
+const config = {
+    creds: {
+        email: 'youremail@email.com',
+        password: 'yourpassword'
+    },
+    target_profile: 'https://www.linkedin.com/in/target-person/',
+};
+
+module.exports = { config };
